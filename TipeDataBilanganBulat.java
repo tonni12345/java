cca439ac779327ea71f997606fbbeae0c4f95cf6
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-06 08:59:15 +0700

    Create TipeDataBilanganBulat.java

---
+++ TipeDataBilanganBulat.java
@@ -0,0 +1,16 @@
+public class TipeDataBilanganBulat {
+    public static void main(String[] args) {
+        // tipe data bilangan bulat
+        // byte ukuran min -128, max 127 size 1 byte
+        byte iniByte = 10;
+
+        // short ukuran min -32768, max 32767 size 2 bytes
+        short iniShort = 1000;
+
+        // int ukuran min -2147483648, max -2147483647 size 4 bytes
+        int iniInt = 100000;
+
+        // long ukuran min -9223372036854775808, max -9223372036854775807 size 8 bytes
+        long iniLong = 1000000000L;
+    }
+}
