e7cc4f2f812fc0f6d4e18e493a8cb3ee88264e92
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-07 13:17:57 +0700

    Create TipeDataBilanganReal.java

---
+++ TipeDataBilanganReal.java
@@ -0,0 +1,7 @@
+class TipeDataBilanganReal{
+    public static void main(String[] args){
+        float iniFloat = 15.99f;
+        // perbedaan nya adalah dari ukuran, dimana double lebih besar dari pada float
+        double iniDouble = 1223.4;
+    }
+}
