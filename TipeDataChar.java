4610bd604330b9e181ec06f208aeb2b63694fd83
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-08 16:30:06 +0700

    Create TipeDataChar.java

---
+++ TipeDataChar.java
@@ -0,0 +1,8 @@
+class TipeDataChar{
+  public static void main(String[] args){
+    // hanya menyimpan satu karakter, berbeda dengan string
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+  }
+}
