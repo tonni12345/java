ff03b17eac06e25450c38a59ba1f5a52e8f2435f
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-09 16:45:02 +0700

    Create TipeDataBoolean.java

---
+++ TipeDataBoolean.java
@@ -0,0 +1,14 @@
+public class TipeDataBoolean {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        if(isTrue){
+            System.out.println("Hello it's true");
+        }
+
+        if(!isFalse){
+            System.out.println("Hello it's false");
+        }
+    }
+}
