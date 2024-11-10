26978fc118c0cc04df91ee86d21ec42436fcfcad
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-10 14:23:52 +0700

    Create TipeDataString.java

---
+++ TipeDataString.java
@@ -0,0 +1,7 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Tonni";
+        String lastName = "Ramdani";
+        System.out.println(firstName+ ' '+ lastName);
+    }
+}
