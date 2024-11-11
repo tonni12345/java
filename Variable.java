b00e06807e6075f16a6d5bed4b8db535cc419fbd
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-11 17:31:12 +0700

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Variable Di Java itu static tidak bisa berubah ubah tipe datanya seperti di bahasa lain
+        // contoh
+        String nama = "Dono";
+        // nama = 10; // akan menimbulkan error int cannot be converted to String // error saat compile time
+        System.out.println(nama);
+    }
+}
