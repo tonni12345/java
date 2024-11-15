29777bd6bbc4620980d541737cda3dab81056440
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-15 10:26:51 +0700

    Create TipeDataPrimitif.java

---
+++ TipeDataPrimitif.java
@@ -0,0 +1,13 @@
+public class TipeDataPrimitif {
+    public static void main(String[] args) {
+        // tipe data primitif adalah tipe data bawaan seperti
+        int noInte = 10;
+        byte noByte = 10;
+        short noShort = 10;
+        long noLong = 10l;
+        float noFlota = 10.1f;
+        double noDouble = 10.1;
+        boolean isPrimitif = true;
+        char firstAbjad = 'A';
+    }
+}
