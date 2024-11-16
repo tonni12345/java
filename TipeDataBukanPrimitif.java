692bff01ce3f5d95255fee768116099dc598da65
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-16 17:06:15 +0700

    Create TipeDataBukanPrimitif.java

---
+++ TipeDataBukanPrimitif.java
@@ -0,0 +1,12 @@
+public class TipeDataBukanPrimitif {
+    public static void main(String[] args) {
+        // tipe data bukan primitif adalah object/class
+        // contoh
+
+        String name = "Eddy Brook";
+        Integer no = 10;
+
+        // tipe data bukan primitif bisa berisi null
+        Boolean isFalse = null;
+    }
+}
