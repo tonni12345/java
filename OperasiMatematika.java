9c818eb6cf0f86d930d6fdbe39b26145368fc712
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-20 13:42:58 +0700

    Create OperasiMatematika.java

---
+++ OperasiMatematika.java
@@ -0,0 +1,20 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+
+        // penjumlahan 
+        System.out.println(a + b);
+
+        // pengurangan
+        System.out.println(a - b);
+
+        // perkalian
+        System.out.println(a * b);
+
+        // pembagian
+        System.out.println(a / b);
+
+        // modulus
+        System.out.println(a % b);
+ }
