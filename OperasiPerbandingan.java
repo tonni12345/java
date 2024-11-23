be2fb91da6da9b6c0946cecf6f39b3de24aee129
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-23 07:07:28 +0700

    Create OperasiPerbandingan.java

---
+++ OperasiPerbandingan.java
@@ -0,0 +1,16 @@
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        // kurang dari
+        System.out.println(2<5); // return true
+        // lebih dari
+        System.out.println(5>2); // return true
+        // kurang dari sama dengan
+        System.out.println(1<=2); //return true
+        // lebih dari sama dengan
+        System.out.println(5>=3); // return true
+        // sama dengan
+        System.out.println(5==5);
+        // ! negasi 
+        System.out.println(!false);
+    }
+}
