c34a63d9fe359b33c1fd37a83fb681bfb3dc66db
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-26 23:28:56 +0700

    Create OperasiBoolean.java

---
+++ OperasiBoolean.java
@@ -0,0 +1,9 @@
+public class OperasiBoolean {
+    public static void main(String[] args) {
+        System.out.println(true && true); // true
+        System.out.println(true && false); // false
+
+        System.out.println(true || true); // true
+        System.out.println(true || false);  // true
+    }
+}
