4cab50f76c5973d09f885163b0af6a17c3556e7b
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-27 14:16:45 +0700

    Create Percabangan.java

---
+++ Percabangan.java
@@ -0,0 +1,10 @@
+public class Percabangan {
+    public static void main(String[] args) {
+        int x = 9;
+        if(x > 10){
+           System.out.println(" lebih besar dari 10"); 
+        } else {
+            System.out.println("X kurang dari atau sama dengan 10");
+        }
+    }
+}
