e67911839d6701801e06f7d19653a8e356e51e20
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-11-30 13:44:53 +0700

    Create TernaryOperator.java

---
+++ TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int nilai = 70;
+
+        String keterangan = nilai >= 75 ? "Selamat Anda Lulus" : "Anda Tidak Lulus";
+
+        System.out.println(keterangan);
+    }
+}
