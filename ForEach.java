f940d9aaa5ddc902fd41e333f5d69d9d93876e16
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-12-03 05:31:23 +0700

    Create ForEach.java

---
+++ ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args){
+        String[] names = {"Tonni", "Ramdani", "Mubaroq", "Nadyra", "Opik", "Nadya"};
+        
+        for(String name : names){
+            System.out.println("Hallo, Selamat Pagi "+ name);
+        }
+    }
+}
