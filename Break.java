2b186fae5bea9764a277761310f921fb1e2fdcb5
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-12-05 18:47:44 +0700

    Create Break.java

---
+++ Break.java
@@ -0,0 +1,10 @@
+public class Break {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++){
+            System.out.print(i);
+            if(i == 5){
+                break; // perulangan akan berhenti di angka lima
+            }
+        }
+    }
+}
