5e6ad90268c3092367a2ce0a032b4827f392a46f
Tonni <49497725+tonni12345@users.noreply.github.com>
2024-12-13 16:26:28 +0700

    Create MethodParameter.java

---
+++ MethodParameter.java
@@ -0,0 +1,9 @@
+class Method {
+    public static void main(String[] args){
+        System.out.println(helloWorld("Dinda"));
+    }
+
+    public static String helloWorld(String name){
+        return "Hello "+ name;
+    }
+}
