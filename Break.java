b62284e9f186ad4186d50c2feca3eb143b932026
Tonni <49497725+tonni1973@users.noreply.github.com>
2025-01-02 15:26:56 +0700

    Update Break.java

--- Break.java
+++ Break.java
@@ -1,9 +1,14 @@
 public class Break {
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 20; i++){
+            i++;
             System.out.print(i);
             if(i == 5){
                 break; // perulangan akan berhenti di angka lima
+            }
+
+            if(i == 10){
+                break;
             }
         }
     }
